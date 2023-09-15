258f5c0ec481ea77bf56ccac69c93104d8a56878
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-09-14 18:16:05 -0600

    finally ?

---
+++ src/index.server.tsx
@@ -0,0 +1,34 @@
+/// <reference types="@rbxts/types/plugin" />
+import App from "components/app";
+import PluginApp from "components/plugin-app";
+import Roact from "@rbxts/roact";
+
+const toolbar = plugin.CreateToolbar("Lily's Plugins");
+const button = toolbar.CreateButton("HmsUtility", "Toggles the utility widget.", "");
+button.ClickableWhenViewportHidden = true;
+
+const dockWidget = plugin.CreateDockWidgetPluginGui(
+	"HmsWidget",
+	new DockWidgetPluginGuiInfo(Enum.InitialDockState.Float, false, false, 300, 200, 300, 200),
+);
+
+function main() {
+	dockWidget.Name = "HmsWidget";
+	dockWidget.Title = "HmsWidget";
+	dockWidget.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+
+	const tree = Roact.mount(
+		<App>
+			<PluginApp key="PluginApp" />
+		</App>,
+		dockWidget,
+		"Main",
+	);
+
+	button.Click.Connect(() => (dockWidget.Enabled = !dockWidget.Enabled));
+	plugin.Unloading.Connect(() => Roact.unmount(tree));
+
+	return 0;
+}
+
+main();
